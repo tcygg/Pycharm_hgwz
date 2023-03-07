dfa79c3aefebb393eaf244b0a5878fb3c3880c79
Tcygg <1179252593@qq.com>
2023-03-08 01:17:24 +0800

    home_one

--- ceshi.py
+++ ceshi.py
@@ -1,1 +1,41 @@
+
+
+e=True
+print(type(e))
+
+
+ee=("sss",11,"s11",True)
+print(type(ee))
+print(ee)
+# djbj1
+
+/**
+djend1
+/
+
+
+print("ss\npp")
+
+
 print(111)
+print(222)
+
+#
+# //dcwhjdc
+#
+# #hfjhfjsf
+# # dkej
+
+
+a=11
+b=22
+print(a)
+
+woshiszhu="jdfjfdhghg"
+
+print(woshiszhu)
+print(woshiszhu[2:7:2])
+
+xusong="wojiushiyige1dashuaieg1"
+
+print(xusong[2])
